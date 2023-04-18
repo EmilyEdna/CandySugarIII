93e84c0d139d13494d6c047032c2da062e4882d7
EmilyEdna <847432003@qq.com>
2023-04-18 17:58:51 +0800

    WPF：自动升级程序

---
+++ PC/CandySugar.ModifyUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: System.Reflection.AssemblyDescription("847432003@qq.com")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.1")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.1")]
+[assembly: System.Reflection.AssemblyCompany("EmilyEdna")]
+[assembly: System.Reflection.AssemblyTitle("甜糖")]
+[assembly: System.Reflection.AssemblyProduct("甜糖")]
+[assembly: System.Reflection.AssemblyCopyright("https://github.com/EmilyEdna")]
