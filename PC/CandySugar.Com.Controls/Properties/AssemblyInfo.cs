28d60976922d1b99137af8ffa487cc10c8622e58
EmilyEdna <847432003@qq.com>
2023-05-16 15:46:02 +0800

    WPF：添加水滴按钮

--- PC/CandySugar.Com.Controls/Properties/AssemblyInfo.cs
+++ PC/CandySugar.Com.Controls/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@
 [assembly: XmlnsDefinition("http://ctrl.candy.com", "CandySugar.Com.Controls.ExtenControls")]
 [assembly: XmlnsDefinition("http://ctrl.candy.com", "CandySugar.Com.Controls.AttachControls")]
 [assembly: XmlnsDefinition("http://ctrl.candy.com", "CandySugar.Com.Controls.UIConverter")]
+[assembly: XmlnsDefinition("http://ctrl.candy.com", "CandySugar.Com.Controls.UIExtenControls")]
 [assembly: System.Reflection.AssemblyDescription("847432003@qq.com")]
 [assembly: System.Reflection.AssemblyFileVersion("1.0.0.1")]
 [assembly: System.Reflection.AssemblyVersion("1.0.0.1")]
