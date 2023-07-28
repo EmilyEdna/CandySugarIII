5cff944fbe1c8e6e267401d4b1a85e194ca1fe9c
EmilyEdna <847432003@qq.com>
2023-07-28 17:58:29 +0800

    WPF：风车动漫组件开发

---
+++ PC/Component/CandySugar.Anime/View/IndexView.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace CandySugar.Anime.View
+{
+    /// <summary>
+    /// IndexView.xaml 的交互逻辑
+    /// </summary>
+    public partial class IndexView : UserControl
+    {
+        public IndexView()
+        {
+            InitializeComponent();
+            GenericDelegate.InformationAction = new((width, height) =>
+            {
+                this.Width = width;
+                this.Height = height - 35 <= 0 ? 0 : height - 35;
+            });
+        }
+    }
+}
---
+++ PC/Component/CandySugar.Anime/ViewModels/IndexViewModel.cs
@@ -0,0 +1,77 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace CandySugar.Anime.ViewModels
+{
+    public class IndexViewModel: PropertyChangedBase
+    {
+        public IndexViewModel()
+        {
+            OnInit();
+        }
+
+        #region Field
+        private int Total;
+        private int PageIndex = 1;
+        #endregion
+
+        #region Property
+        private ObservableCollection<CartInitElementResult> _InitResult;
+        public ObservableCollection<CartInitElementResult> InitResult
+        {
+            get => _InitResult;
+            set=>SetAndNotify(ref _InitResult, value);
+        }
+        #endregion
+
+        #region  Method
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        private void OnInit()
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    var result = (await CartFactory.Car(opt =>
+                    {
+                        opt.RequestParam = new Input
+                        {
+                            CacheSpan = ComponentBinding.OptionObjectModels.Cache,
+                            CartType = CartEnum.Init,
+                             Init=new CartInit()
+                        };
+                    }).RunsAsync()).InitResult;
+                    Total=result.Total;
+                    InitResult = new ObservableCollection<CartInitElementResult>(result.ElementResults);
+                }
+                catch (Exception ex)
+                {
+                    Log.Logger.Error(ex, "");
+                    ErrorNotify();
+                }
+            });
+        }
+
+        private void ErrorNotify()
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                new ScreenNotifyView(CommonHelper.ComponentErrorInformation).Show();
+            });
+        }
+        #endregion
+
+        #region Command
+        /// <summary>
+        /// 加载更多
+        /// </summary>
+        public RelayCommand<ScrollChangedEventArgs> ScrollCommand => new((obj) =>
+        {
+
+        });
+
+        #endregion
+    }
+}
