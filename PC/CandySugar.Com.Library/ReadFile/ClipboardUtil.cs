59b10e346fc68d5425e38feae7a48f49afb6ef4a
EmilyEdna <847432003@qq.com>
2023-07-31 17:42:01 +0800

    WPF：处理线程取消BUG

--- PC/CandySugar.Com.Library/ReadFile/ClipboardUtil.cs
+++ PC/CandySugar.Com.Library/ReadFile/ClipboardUtil.cs
@@ -1,24 +1,33 @@
 ﻿using CandySugar.Com.Options.ComponentGeneric;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using XExten.Advance.ThreadFramework;
 
 namespace CandySugar.Com.Library.ReadFile
 {
     public class ClipboardUtil
     {
+     
         public static void InitClipBoard()
         {
             ThreadFactory.Instance.StartWithRestart(() =>
             {
-                Application.Current.Dispatcher.Invoke(() =>
+                try
                 {
-                    if (Clipboard.ContainsText())
-                        if (Clipboard.GetText().Contains("www.bilibili.com/video"))
-                            GenericDelegate.ClipboardAction?.Invoke(Clipboard.GetText());
-                });
-                Thread.Sleep(300);
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (Clipboard.ContainsText())
+                            if (Clipboard.GetText().Contains("www.bilibili.com/video"))
+                                GenericDelegate.ClipboardAction?.Invoke(Clipboard.GetText());
+                    });
+                    Thread.Sleep(300);
+                }
+                catch
+                {
+                }
             }, "ClipBoardCheck", true);
 
         }
