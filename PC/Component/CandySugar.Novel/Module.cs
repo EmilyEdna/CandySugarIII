4a6ab71b5cd4c173d4fe48727a5128daa3a41209
EmilyEdna <847432003@qq.com>
2023-08-31 15:21:42 +0800

    WPF：小说插件开发

---
+++ PC/Component/CandySugar.Novel/Module.cs
@@ -0,0 +1,23 @@
+﻿namespace CandySugar.Novel
+{
+    public class Module
+    {
+        public static Module IocModule { get; set; }
+        private static IContainer Container { get; set; }
+        public Module()
+        {
+            IocModule = this;
+            Container = new Container();
+            Container.Register(typeof(IndexView), Reuse.Singleton);
+
+            Container.Register(typeof(IndexViewModel), Reuse.Singleton);
+        }
+        public T Resolve<T>() where T : UserControl
+        {
+            var Ctrl = (UserControl)Container.Resolve(typeof(T));
+            var VM = this.GetType().Assembly.GetTypes().FirstOrDefault(t => t.Name == $"{typeof(T).Name}Model");
+            Ctrl.DataContext = Container.Resolve(VM);
+            return (T)Ctrl;
+        }
+    }
+}
