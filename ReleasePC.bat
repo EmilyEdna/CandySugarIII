32492924eee00e80afe4bd0dfd73bcb75b3a1dc8
EmilyEdna <847432003@qq.com>
2023-09-23 16:50:32 +0800

    WPF：修改发布脚本

--- ReleasePC.bat
+++ ReleasePC.bat
@@ -4,6 +4,7 @@ cd /d %~dp0
 dotnet publish PC\CandySugar.MainUI\CandySugar.MainUI.csproj -c Release -o ..\CandySugar\Release -f net7.0-windows --sc true -r win-x64 /p:DebugType=None /p:DebugSymbols=false
 dotnet publish PC\CandySugar.ModifyUI\CandySugar.ModifyUI.csproj -c Release -o ..\CandySugar\Release -f net7.0-windows --sc true -r win-x64 /p:DebugType=None /p:DebugSymbols=false
 dotnet publish PC\Component\CandySugar.LightNovel\CandySugar.LightNovel.csproj -c Release -o ..\CandySugar\Release -f net7.0-windows --sc true -r win-x64 /p:DebugType=None /p:DebugSymbols=false
+dotnet publish PC\Component\CandySugar.Novel\CandySugar.Novel.csproj -c Release -o ..\CandySugar\Release -f net7.0-windows --sc true -r win-x64 /p:DebugType=None /p:DebugSymbols=false
 dotnet publish PC\Component\CandySugar.Music\CandySugar.Music.csproj -c Release -o ..\CandySugar\Release -f net7.0-windows --sc true -r win-x64 /p:DebugType=None /p:DebugSymbols=false
 dotnet publish PC\Component\CandySugar.WallPaper\CandySugar.WallPaper.csproj -c Release -o ..\CandySugar\Release -f net7.0-windows --sc true -r win-x64 /p:DebugType=None /p:DebugSymbols=false
 dotnet publish PC\Component\CandySugar.Bilibili\CandySugar.Bilibili.csproj -c Release -o ..\CandySugar\Release -f net7.0-windows --sc true -r win-x64 /p:DebugType=None /p:DebugSymbols=false
@@ -24,6 +25,8 @@ rd /S /Q PC\CandySugar.Com.Options\obj PC\CandySugar.Com.Options\bin\Release
 rd /S /Q PC\CandySugar.Com.Style\obj PC\CandySugar.Com.Style\bin\Release
 
 rd /S /Q PC\Component\CandySugar.LightNovel\obj PC\Component\CandySugar.LightNovel\bin\Release
+
+rd /S /Q PC\Component\CandySugar.Novel\obj PC\Component\CandySugar.Novel\bin\Release
 
 rd /S /Q PC\Component\CandySugar.Music\obj PC\Component\CandySugar.Music\bin\Release
 
