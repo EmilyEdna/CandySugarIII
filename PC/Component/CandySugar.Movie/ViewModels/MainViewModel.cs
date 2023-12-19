92628da17f6d640e5101d4133213a1259688c161
EmilyEdna <847432003@qq.com>
2023-12-19 17:38:23 +0800

    WPF：初始化影剧

---
+++ PC/Component/CandySugar.Movie/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CandySugar.Movie.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandySugar.Movie.ViewModels
+{
+    public class MainViewModel: PropertyChangedBase
+    {
+        public MainViewModel()
+        {
+            ComponentControl = Module.IocModule.Resolve<IndexView>();
+        }
+        #region Property
+        private Control _ComponentControl;
+        public Control ComponentControl
+        {
+            get => _ComponentControl;
+            set => SetAndNotify(ref _ComponentControl, value);
+        }
+        #endregion
+    }
+}
