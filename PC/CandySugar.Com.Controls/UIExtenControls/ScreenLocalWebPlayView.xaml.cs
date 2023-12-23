8fa940033c4c6472134f26bfd72fbac22b1770e7
EmilyEdna <847432003@qq.com>
2023-12-23 20:18:21 +0800

    WPF：修复webview2加载html过快导致JS无法执行

--- PC/CandySugar.Com.Controls/UIExtenControls/ScreenLocalWebPlayView.xaml.cs
+++ PC/CandySugar.Com.Controls/UIExtenControls/ScreenLocalWebPlayView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using CandySugar.Com.Library.KeepOn;
+using Microsoft.Web.WebView2.Core;
+using Serilog;
 using System;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,33 +20,30 @@ namespace CandySugar.Com.Controls.UIExtenControls
             InitWebView(playroute);
         }
 
-        private  void InitWebView(string playroute)
+        private void InitWebView(string playroute)
         {
-            WebPlayer.Source= new Uri(playroute);
-            WebPlayer.NavigationCompleted += async delegate
-            {
-                await WebPlayer.EnsureCoreWebView2Async();
-                WebPlayer.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
-                WebPlayer.CoreWebView2.Settings.AreDevToolsEnabled = true;
-                await this.Dispatcher.BeginInvoke(Dotry);
-            };
+            WebPlayer.Source = new Uri(playroute);
+            WebPlayer.NavigationCompleted += PlayLoadEvent;
         }
 
-        private async void Dotry()
+        private async void PlayLoadEvent(object sender, CoreWebView2NavigationCompletedEventArgs e)
         {
-            while (true) 
+            await WebPlayer.EnsureCoreWebView2Async();
+            WebPlayer.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+            WebPlayer.CoreWebView2.Settings.AreDevToolsEnabled = true;
+            await this.Dispatcher.BeginInvoke(async () =>
             {
                 var data = await WebPlayer.CoreWebView2.ExecuteScriptAsync("$('iframe')[1].contentWindow.config.url");
                 if (data != "null" && data.Contains(".m3u8"))
                 {
+                    Log.Logger.Debug($"流媒体地址：{data.Replace("\"", "")}");
                     WebPlayer.CoreWebView2.Navigate(new Uri($"{Environment.CurrentDirectory}\\Assets\\Player.html").AbsoluteUri);
-                    await WebPlayer.CoreWebView2.ExecuteScriptAsync($"Play('{data.Replace("\"", "")}')");
-                    break;
+                    await Task.Delay(2000); //等待html加载完成
+                    await WebPlayer.CoreWebView2.ExecuteScriptAsync($"opt.uri='{data.Replace("\"", "")}'");
                 }
-                else await Task.Delay(1000);
-            }
+            });
         }
-        
+
         private void Window_Closed(object sender, EventArgs e)
         {
             ScreenKeep.RestoreForCurrentThread();
