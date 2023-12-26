8bae997c2cda19d3852f86e15e02b1f48397d037
EmilyEdna <847432003@qq.com>
2023-12-26 10:45:52 +0800

    WPF：插件构建Bat

---
+++ PC/CandySugar.MainUI/PluginBuild.bat
@@ -0,0 +1,15 @@
+chcp 65001
+echo Start Build
+if "%1"=="Debug" (
+  echo Plugin Build
+  (for %%a in (LightNovel,Novel,Movie,Music,Bilibili) do (
+	echo Begin Build %%a
+	dotnet build ../Component/CandySugar.%%a/CandySugar.%%a.csproj --no-dependencies  -c %1
+  ))
+  (for %%b in (WallPaper,Anime,Rifan,Comic,Axgle) do (
+	echo Begin Build %%b
+	dotnet build ../Component/CandySugar.%%b/CandySugar.%%b.csproj --no-dependencies  -c %1
+  ))
+  dotnet build ../CandySugar.ModifyUI/CandySugar.ModifyUI.csproj --no-dependencies  -c %1
+  echo Build Complete
+)
