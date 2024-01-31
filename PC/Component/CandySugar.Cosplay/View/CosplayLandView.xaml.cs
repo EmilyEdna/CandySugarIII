2632d8d32985458bf17170ca01806563c700d642
EmilyEdna <847432003@qq.com>
2024-01-31 18:03:03 +0800

    WPF：Cos插件

--- PC/Component/CandySugar.Cosplay/View/CosplayLandView.xaml.cs
+++ PC/Component/CandySugar.Cosplay/View/CosplayLandView.xaml.cs
@@ -1,28 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace CandySugar.Cosplay.View
+﻿namespace CandySugar.Cosplay.View
 {
     /// <summary>
     /// CosplayLandView.xaml 的交互逻辑
     /// </summary>
     public partial class CosplayLandView : UserControl
     {
+        public int ActiveAnime = 1;
+        public Storyboard AnimeY1;
+        public Storyboard AnimeY2;
+        private CosplayLandViewModel ViewModel;
         public CosplayLandView()
         {
             InitializeComponent();
+            Loaded += delegate { ViewModel = (CosplayLandViewModel)this.DataContext; };
+            AnimeY1 = (Storyboard)FindResource("Y1Key");
+            AnimeY2 = (Storyboard)FindResource("Y2Key");
+            AnimeY1.Completed += CompletedEvent;
+            AnimeY2.Completed += CompletedEvent;
+            WeakReferenceMessenger.Default.Register<MessageNotify>(this, (recip, notify) =>
+            {
+                if (notify.ControlParam is Tuple<double, double> data)
+                {
+                    this.Width = data.Item1;
+                    this.Height = data.Item2;
+                }
+                else
+                    ViewModel.Builder.Clear();
+            });
+        }
+
+        private void CompletedEvent(object sender, EventArgs e)
+        {
+            ViewModel.ChangeCommand(ActiveAnime);
         }
     }
 }
--- PC/Component/CandySugar.Cosplay/ViewModels/CosplayLandViewModel.cs
+++ PC/Component/CandySugar.Cosplay/ViewModels/CosplayLandViewModel.cs
@@ -1,12 +1,203 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XExten.Advance.JsonDbFramework;
 
 namespace CandySugar.Cosplay.ViewModels
 {
     public class CosplayLandViewModel: PropertyChangedBase
     {
+        private object LockObject = new object();
+        public List<CosplayInitElementResult> Builder;
+        private JsonDbHandle<CosplayInitElementResult> JsonHandler;
+        private string DbPath = Path.Combine(CommonHelper.DownloadPath, "Cosplay", $"CosplayLand.{FileTypes.Dat}");
+        public CosplayLandViewModel()
+        {
+            Title = ["常规", "收藏"];
+            JsonHandler = new JsonDbContext(DbPath).LoadInMemory<CosplayInitElementResult>();
+            var LocalDATA = JsonHandler.GetAll();
+            CollectResult = new ObservableCollection<CosplayInitElementResult>();
+            if (LocalDATA != null)
+            {
+                LocalDATA.ForEach(CollectResult.Add);
+            }
+        }
+
+        #region Field
+        private string Keyword;
+        private int GeneralTotal;
+        private int GeneralPageIndex = 1;
+        /// <summary>
+        /// 1：常规  2：收藏
+        /// </summary>
+        private int ChangeType = 1;
+        #endregion
+
+        #region Property
+        private ObservableCollection<string> _Title;
+        public ObservableCollection<string> Title
+        {
+            get => _Title;
+            set => SetAndNotify(ref _Title, value);
+        }
+
+        private ObservableCollection<CosplayInitElementResult> _CosResult;
+        public ObservableCollection<CosplayInitElementResult> CosResult
+        {
+            get => _CosResult;
+            set => SetAndNotify(ref _CosResult, value);
+        }
+
+        private ObservableCollection<CosplayInitElementResult> _CollectResult;
+        public ObservableCollection<CosplayInitElementResult> CollectResult
+        {
+            get => _CollectResult;
+            set => SetAndNotify(ref _CollectResult, value);
+        }
+        #endregion
+
+        #region Command
+        public RelayCommand<object> ChangedCommand => new((item) =>
+        {
+            var Target = ((CandyToggleItem)item);
+            if (Target.FindParent<UserControl>() is CosplayLandView View)
+            {
+                var Index = Target.Tag.ToString().AsInt();
+
+                if (Index == 0)
+                {
+                    View.ActiveAnime = 1;
+                    View.AnimeY1.Begin();
+                }
+                if (Index == 1)
+                {
+                    View.ActiveAnime = 2;
+                    View.AnimeY2.Begin();
+                }
+            }
+        });
+        /// <summary>
+        /// 切换
+        /// </summary>
+        /// <param name="type"></param>
+        public void ChangeCommand(int type)
+        {
+            ChangeType = type;
+            if (ChangeType == 1 && CosResult == null)
+                OnCosInit();
+        }
+        /// <summary>
+        /// 加载更多
+        /// </summary>
+        public RelayCommand<ScrollChangedEventArgs> ScrollCommand => new((obj) =>
+        {
+            if (ChangeType == 1)
+            {
+                if (GeneralPageIndex <= GeneralTotal && obj.VerticalOffset + obj.ViewportHeight == obj.ExtentHeight && obj.VerticalChange > 0)
+                {
+                    GeneralPageIndex += 1;
+                    OnLoadMoreCosInit();
+                }
+            }
+        });
+        /// <summary>
+        /// 收藏
+        /// </summary>
+        /// <param name="element"></param>
+        public void CollectCommand(CosplayInitElementResult element)
+        {
+            CollectResult.Add(element);
+            JsonHandler.Insert(element).ExuteInsert().SaveChange();
+        }
+        public void CheckCommand(CosplayInitElementResult input)
+        {
+            Builder.Add(input);
+            GenericDelegate.HandleAction?.Invoke(Builder);
+        }
+        public void UnCheckCommand(CosplayInitElementResult input)
+        {
+            Builder.Remove(input);
+            GenericDelegate.HandleAction?.Invoke(Builder);
+        }
+        #endregion
+
+        #region Method
+        private void ErrorNotify(string Info = "")
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                new ScreenNotifyView(Info.IsNullOrEmpty() ? CommonHelper.ComponentErrorInformation : Info).Show();
+            });
+        }
+
+        private void OnCosInit()
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    var Proxy = Module.IocModule.Proxy;
+                    var result = (await CosplayFactory.Cosplay(opt =>
+                    {
+                        opt.RequestParam = new Input
+                        {
+                            ProxyIP = Proxy.IP,
+                            ProxyPort = Proxy.Port,
+                            CacheSpan = ComponentBinding.OptionObjectModels.Cache,
+                            CosplayType = CosplayEnum.Init,
+                            PlatformType = PlatformEnum.Land,
+                            Init = new CosplayInit
+                            {
+                                Page = 1
+                            }
+                        };
+                    }).RunsAsync()).InitResult;
+                    GeneralTotal = result.Total;
+                    CosResult = new ObservableCollection<CosplayInitElementResult>(result.ElementResults);
+                }
+                catch (Exception ex)
+                {
+                    Log.Logger.Error(ex, "");
+                    ErrorNotify();
+                }
+            });
+        }
+
+        private void OnLoadMoreCosInit()
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    var Proxy = Module.IocModule.Proxy;
+                    var result = (await CosplayFactory.Cosplay(opt =>
+                    {
+                        opt.RequestParam = new Input
+                        {
+                            ProxyIP = Proxy.IP,
+                            ProxyPort = Proxy.Port,
+                            CacheSpan = ComponentBinding.OptionObjectModels.Cache,
+                            CosplayType = CosplayEnum.Init,
+                            PlatformType = PlatformEnum.Land,
+                            Init = new CosplayInit
+                            {
+                                Page = GeneralPageIndex
+                            }
+                        };
+                    }).RunsAsync()).InitResult;
+                    BindingOperations.EnableCollectionSynchronization(CosResult, LockObject);
+                    Application.Current.Dispatcher.Invoke(() => result.ElementResults.ForEach(CosResult.Add));
+                }
+                catch (Exception ex)
+                {
+                    Log.Logger.Error(ex, "");
+                    ErrorNotify();
+                }
+            });
+        }
+        #endregion
     }
 }
