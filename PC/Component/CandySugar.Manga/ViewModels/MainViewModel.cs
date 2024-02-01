9400426930b80212105262ade3e36f102b10d812
EmilyEdna <847432003@qq.com>
2024-02-01 14:42:09 +0800

    WPF：新增漫画插件

---
+++ PC/Component/CandySugar.Manga/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandySugar.Manga.ViewModels
+{
+    public class MainViewModel: PropertyChangedBase
+    {
+        public MainViewModel()
+        {
+            ComponentControl = Module.IocModule.Resolve<IndexView>();
+        }
+        #region Property
+        private Control _ComponentControl;
+        public Control ComponentControl
+        {
+            get => _ComponentControl;
+            set => SetAndNotify(ref _ComponentControl, value);
+        }
+        #endregion
+    }
+}
