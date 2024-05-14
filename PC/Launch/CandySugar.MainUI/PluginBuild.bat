b7d1bc13e79c70915789d41c81bb538e67dd120d
EmilyEdna <847432003@qq.com>
2024-05-14 20:09:17 +0800

    WPF：修改生产路径

--- PC/Launch/CandySugar.MainUI/PluginBuild.bat
+++ PC/Launch/CandySugar.MainUI/PluginBuild.bat
@@ -4,11 +4,11 @@ if "%1"=="Debug" (
   echo Plugin Build
   (for %%a in (LightNovel,Novel,Movie,Music,Bilibili,Manga) do (
 	echo Begin Build %%a
-	dotnet build ../Component/CandySugar.%%a/CandySugar.%%a.csproj --no-dependencies  -c %1
+	dotnet build ../../Component/CandySugar.%%a/CandySugar.%%a.csproj --no-dependencies  -c %1
   ))
   (for %%b in (WallPaper,Anime,Rifan,Comic,Axgle Cosplay) do (
 	echo Begin Build %%b
-	dotnet build ../Component/CandySugar.%%b/CandySugar.%%b.csproj --no-dependencies  -c %1
+	dotnet build ../../Component/CandySugar.%%b/CandySugar.%%b.csproj --no-dependencies  -c %1
   ))
   dotnet build ../CandySugar.ModifyUI/CandySugar.ModifyUI.csproj --no-dependencies  -c %1
   echo Build Complete
