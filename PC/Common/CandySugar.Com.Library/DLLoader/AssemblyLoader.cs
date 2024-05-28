1611c5cce5c52cc23d50fdd516f515ca15d50bc0
EmilyEdna <847432003@qq.com>
2024-05-28 16:39:33 +0800

    WPF：重写加载插件

--- PC/Common/CandySugar.Com.Library/DLLoader/AssemblyLoader.cs
+++ PC/Common/CandySugar.Com.Library/DLLoader/AssemblyLoader.cs
@@ -9,123 +9,67 @@ using System.Runtime.Loader;
 
 namespace CandySugar.Com.Library.DLLoader
 {
-    public class AssemblyLoader
+    public class AssemblyLoader : AssemblyLoadContext
     {
-        private string _basePath;
-        private AssemblyLoadContext context;
+        private string _Path;
         public static ConcurrentQueue<Type> Single = new ConcurrentQueue<Type>();
         public static List<DLLInformations> Dll = new List<DLLInformations>();
-        public AssemblyLoader(string basePath)
+        public AssemblyLoader(string Path)
         {
-            _basePath = basePath;
+            _Path = Path;
+            this.Resolving += DllResolving;
         }
-        /// <summary>
-        /// 动态加载Dll
-        /// </summary>
-        /// <param name="dllFileName"></param>
-        /// <param name="typeName"></param>
-        public void Load(string dllFileName, string typeName)
-        {
-            context = new AssemblyLoadContext(dllFileName);
-            context.Resolving += Context_Resolving;
-            //需要绝对路径
-            string path = Path.Combine(_basePath, dllFileName);
-            if (File.Exists(path))
-            {
-                try
-                {
-                    using (var stream = File.OpenRead(path))
-                    {
-                        Assembly assembly = context.LoadFromStream(stream);
-                        Type InstanceType = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower().Equals(typeName.ToLower()));
-                        Single.Enqueue(InstanceType);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"加载节点{dllFileName}-{typeName}发生异常：{ex.Message},{ex.StackTrace}");
-                }
 
-            }
-            else
-            {
-                Console.WriteLine($"节点动态库{dllFileName}不存在：{path}");
-            }
-        }
-        /// <summary>
-        /// 动态加载Dll
-        /// </summary>
-        /// <param name="dllFileName"></param>
-        /// <param name="typeName"></param>
-        /// <param name="configTypeName"></param>
-        /// <param name="description"></param>
-        public void Load(string dllFileName, string typeName, string ioc = "Module", string description = "")
-        {
-            context = new AssemblyLoadContext(dllFileName);
-            context.Resolving += Context_Resolving;
-            //需要绝对路径
-            string path = Path.Combine(_basePath, dllFileName);
-            if (File.Exists(path))
-            {
-                try
-                {
-                    using (var stream = File.OpenRead(path))
-                    {
-                        Assembly assembly = context.LoadFromStream(stream);
-                        Type InstanceType = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower().Equals(typeName.ToLower()));
-                        Type ViewModel = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower().Contains($"{typeName}Model".ToLower()));
-                        Type IocModule = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower().Equals(ioc.ToLower()));
-                        Dll.Add(new DLLInformations
-                        {
-                            InstanceViewModel = ViewModel,
-                            InstanceType = InstanceType,
-                            IocModule = IocModule,
-                            Description = description,
-                            IsEnable = true,
-                        });
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Log.Logger.Error($"加载节点{dllFileName}-{typeName}发生异常：{ex.Message},{ex.StackTrace}");
-                }
-
-            }
-            else
-            {
-                Log.Logger.Error($"节点动态库{dllFileName}不存在：{path}");
-            }
-        }
-        /// <summary>
-        /// 加载依赖文件
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="assemblyName"></param>
-        /// <returns></returns>
-        private Assembly Context_Resolving(AssemblyLoadContext context, AssemblyName assemblyName)
+        #region Event
+        private Assembly DllResolving(AssemblyLoadContext context, AssemblyName name)
         {
             string[] Filter = { $"{Path.GetFileNameWithoutExtension(context.Name)}.resources" };
-            if (Filter.Contains(assemblyName.Name)) return null;
-            string expectedPath = Path.Combine(_basePath, assemblyName.Name + ".dll"); ;
-            if (File.Exists(expectedPath))
-            {
-                try
-                {
-                    using (var stream = File.OpenRead(expectedPath))
-                    {
-                        return context.LoadFromStream(stream);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Log.Logger.Error($"加载节点{expectedPath}发生异常：{ex.Message},{ex.StackTrace}");
-                }
-            }
-            else
-            {
-                Log.Logger.Error($"依赖文件不存在：{expectedPath}");
-            }
+            if (Filter.Contains(name.Name)) return null;
+            string expectedPath = Path.Combine(_Path, name.Name + ".dll");
+            if (File.Exists(expectedPath)) return context.LoadFromAssemblyPath(expectedPath);
+            Log.Logger.Error($"依赖文件不存在：{expectedPath}");
             return null;
+        }
+        #endregion
+
+        public void Loads(string dllFileName, string typeName, string ioc = "Module", string description = "")
+        {
+            try
+            {
+                string path = Path.Combine(_Path, dllFileName);
+                var assembly = this.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(path)));
+                Type InstanceType = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower().Equals(typeName.ToLower()));
+                Type ViewModel = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower().Contains($"{typeName}Model".ToLower()));
+                Type IocModule = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower().Equals(ioc.ToLower()));
+                Dll.Add(new DLLInformations
+                {
+                    InstanceViewModel = ViewModel,
+                    InstanceType = InstanceType,
+                    IocModule = IocModule,
+                    Description = description,
+                    IsEnable = true,
+                });
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex,$"当前资源不存在：{ex.Message}");
+            }
+        }
+
+
+        public void Load(string dllFileName, string typeName) 
+        {
+            try
+            {
+                string path = Path.Combine(_Path, dllFileName);
+                var assembly = this.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(path)));
+                Type InstanceType = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower().Equals(typeName.ToLower()));
+                Single.Enqueue(InstanceType);
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex, $"当前资源不存在：{ex.Message}");
+            }
         }
     }
 }
